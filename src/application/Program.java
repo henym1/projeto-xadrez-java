0011cdc43ddb8d90228f8e2452e394dc5222de01
Henry Adam <iihenymii@gmail.com>
2022-07-21 10:30:08 -0300

    criada classe Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Vamo gurizada");
-
+		
+		
+		Position pos = new Position(2, 4);
+		System.out.println(pos);
+		
+		
 	}
 
 }
